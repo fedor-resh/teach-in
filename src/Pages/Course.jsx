d86214df377e2e3c6039151f270c4d5323a04390
feduk <fedor-resh@mail.ru>
2024-04-22 01:12:43 +0300

    add base

---
+++ src/Pages/Course.jsx
@@ -0,0 +1,75 @@
+import {Card, Group, Space, Title, Text, Image} from "@mantine/core";
+import {useParams} from "react-router-dom";
+
+const mockCourse = {
+    id: 1,
+    name: "Course 1",
+    image: "https://via.placeholder.com/150",
+    lecturer: "Lecturer 1",
+    lectures: [
+        {
+            id: 1,
+            name: "Lecture 1",
+            video: "https://www.youtube.com/watch?v=6IwUl-4o5jI"
+        },
+        {
+            id: 2,
+            name: "Lecture 2",
+            video: "https://www.youtube.com/watch?v=6IwUl-4o5jI"
+        },
+        {
+            id: 3,
+            name: "Lecture 3",
+            video: "https://www.youtube.com/watch?v=6IwUl-4o5jI"
+        }
+    ],
+    materials: [
+        {
+            id: 1,
+            name: "Material 1",
+            link: "https://www.youtube.com/watch?v=6IwUl-4o5jI"
+        },
+        {
+            id: 2,
+            name: "Material 2",
+            link: "https://www.youtube.com/watch?v=6IwUl-4o5jI"
+        },
+        {
+            id: 3,
+            name: "Material 3",
+            link: "https://www.youtube.com/watch?v=6IwUl-4o5jI"
+        }
+    ]
+}
+export function Course() {
+    const {id: courseId} = useParams()
+    return (
+        <>
+            <Title>{mockCourse.name}</Title>
+            <Space h="md"/>
+            <Title order={5}>Lecturer: {mockCourse.lecturer}</Title>
+            <Space h="md"/>
+            {mockCourse.lectures.map(lecture => (
+                <>
+                    <Card withBorder shadow="sm" p="lg" key={lecture.id}>
+                        <Group align="start">
+                            <Image src={mockCourse.image} alt={mockCourse.name}/>
+                            <Text weight={500} size="lg" mt="md">
+                                {lecture.name}
+                            </Text>
+                        </Group>
+                    </Card>
+                    <Space h="md"/>
+                </>
+            ))}
+            {mockCourse.materials.map(material => (
+                <>
+                    <a href={material.link}>
+                        {material.name}
+                    </a>
+                    <Space h="md"/>
+                </>
+            ))}
+        </>
+    )
+}
