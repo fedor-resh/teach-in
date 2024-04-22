9cb7d4c25db58807a02d22684cded01e2ce018de
feduk <fedor-resh@mail.ru>
2024-04-22 23:55:28 +0300

    dark theme

--- src/main.jsx
+++ src/main.jsx
@@ -51,7 +51,7 @@ const theme = createTheme({
 })
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <MantineProvider theme={theme}>
+        <MantineProvider theme={theme} defaultColorScheme='dark'>
             <RouterProvider router={router} />
         </MantineProvider>
     </React.StrictMode>,
